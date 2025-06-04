3f403907eea33bb6257ac227e7a4884a992298e9
RivuParth <user@example.com>
2025-06-04 17:55:29 +0530

    Add Terraform changes

--- infrastructuress/security_groups.tf
+++ infrastructuress/security_groups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]  # Change to your IP for better security
+    cidr_blocks      = ["0.0.0.0/0"] 
   }
 
   ingress {
