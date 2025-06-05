1278f19ec16ab2001594abc6ba510d52e5ab8185
RivuParth <user@example.com>
2025-06-05 14:53:47 +0530

    Infra AWS Terraform Testing 02

--- app/startup_script.sh
+++ app/startup_script.sh
@@ -15,11 +15,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! Partha'
+    return '''
+    <html>
+    <head>
+        <title>Hello Partha</title>
+        <style>
+            body {
+                background-color: #f0f8ff;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                font-family: Arial, sans-serif;
+            }
+            h1 {
+                font-size: 60px;
+                color: #2c3e50;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello World!<br>From Partha 🚀</h1>
+    </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 EOF
+
 
 # Run Flask 
 python3 app.py &
